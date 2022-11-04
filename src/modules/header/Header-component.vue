2e28175cd753df95b164678caa3d68c402cace5e
Dastan Kadylbekov <dastan.962601@gmail.com>
2022-11-04 21:15:35 +0600

    start project

---
+++ src/modules/header/Header-component.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <div class="header-wrapper">
+        <div class="header-logo">Logo</div>
+        <nav class="nav">
+          <div class="nav-menu">
+            <li class="nav-menu__item">
+              <a href="" class="nav-menu__item-link">About</a>
+            </li>
+            <li class="nav-menu__item">
+              <a href="" class="nav-menu__item-link">News</a>
+            </li>
+            <li class="nav-menu__item">
+              <a href="" class="nav-menu__item-link">Contacts</a>
+            </li>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+@import "../../assets/scss/header.scss";
+</style>
